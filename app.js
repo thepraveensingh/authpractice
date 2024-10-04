18f083a93e2f34fc2ca38fce29dc84de29a70d03
thepraveensingh <2021kuec2066@iiitkota.ac.in>
2024-10-04 16:09:41 +0530

    mongodb connected and form created

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser')
+const userModel = require('./models/user')
+
+app.set("view engine","ejs")
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname,'public')));
+
 app.use(cookieParser());
+
 //// encrypt
 // app.get('/',(req,res)=>{
 //    bcrypt.genSalt(10,(err,salt)=>{
@@ -24,19 +33,39 @@ app.use(cookieParser());
 //   }) 
 // })
 
-
+////json web token
+// app.get('/',(req,res)=>{
+//   let token = jwt.sign({email:"praveens@gmail.com"},"secret");
+//   res.cookie("token",token)
+//   console.log(token);
+//   res.send("done");
+// })
+// app.get('/read',(req,res)=>{
+//   let data = jwt.verify(req.cookies.token,"secret");
+//   console.log(data);
+// })
 app.get('/',(req,res)=>{
-  let token = jwt.sign({email:"praveens@gmail.com"},"secret");
-  res.cookie("token",token)
-  console.log(token);
-  res.send("done");
+  res.render('index');
 })
-app.get('/read',(req,res)=>{
-  let data = jwt.verify(req.cookies.token,"secret");
-  console.log(data);
-  
+app.post('/create',async(req,res)=>{
+  let{username, email , password,age} = req.body;
+  let createdUser = await userModel.create({
+    username,
+    email,
+    password,
+    age
+  })
+  res.send(createdUser);
 })
 app.listen(3000,()=>{
   console.log("server at 3000");
-  
 });
+
+////flow
+//create user account
+//mongoose
+//schema
+//model
+//usercreate -> password -> hash
+//jwt token ->cookie
+//login -> token -> decrypt -> email
